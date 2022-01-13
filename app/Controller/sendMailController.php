462a36f45e3d07d79391ca10948bf60aa65aa89f
max832 <60100632+max832@users.noreply.github.com>
2022-01-13 16:24:14 +0100

    Update sendMailController.php

--- app/Controller/sendMailController.php
+++ app/Controller/sendMailController.php
@@ -3,21 +3,21 @@ namespace app\Controller;
 
 class sendMailController
 {
-    public function sendMailTo($nom, $mail, $message)
-    {
+public function sendMailTo($nom, $mail, $message)
+{
 
-    $mailTo = "adrien.houee1@gmail.com";
+$mailTo = "abpiz154@gmail.com";
 
-    $entete  = 'MIME-Version: 1.0' . "\r\n";
-        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $entete .= 'From: ' . $mailsender . "\r\n";
+$entete  = 'MIME-Version: 1.0' . "\r\n";
+    $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $entete .= 'From: ' . $mailsender . "\r\n";
 
-        $commentaire = '<h1>Prise de contact via le formaulaire</h1>
-         <p><b>Nom du contact : </b>' . $nom . '<br>
-         <p><b>Email du contact : </b>' . $mail . '<br>
-         <p><b>Message : </b>' . $message . '</p>';
+    $commentaire = '<h1>Prise de contact via le formaulaire</h1>
+        <p><b>Nom du contact : </b>' . $nom . '<br>
+        <p><b>Email du contact : </b>' . $mail . '<br>
+        <p><b>Message : </b>' . $message . '</p>';
 
-        $retour = mail($mailTo, 'Pizza Lab - Contact', $commentaire, $entete);
+    $retour = mail($mailTo, 'Pizza Lab - Contact', $commentaire, $entete);
         if($retour) {
             header("Location: index.php?sendMail");
         }else{
